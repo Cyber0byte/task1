23954da36e437acfac81892346267f69ea5c680c
Cyber <savbusines2@ya.ru>
2024-11-26 00:08:00 +0700

    secong commit

--- solution.h
+++ solution.h
@@ -3,45 +3,41 @@
 #include <stdio.h>
 #include <math.h>
 
-// int cyclic_right_shift(int num)
-// {
-//     int digits = (int)log10(num); // Считаем количество цифр в числе
-
-//     int last_part = num % 10;
-//     int first_part = num / 10;
-
-//     return last_part * (pow(10, digits)) + first_part;
-// }
-
-
-int cyclic_right_shift(int num, int shift) {
+int cyclic_right_shift(int num, int shift)
+{
     // Считаем количество разрядов в числе
     int digits = (int)log10(num) + 1;
-    printf("%d\n", digits);
-    
+
     // Приводим сдвиг к диапазону 0 - digits - 1
     shift = shift % digits;
 
     // Если сдвиг нулевой, возвращаем исходное число
-    if (shift == 0) {
+    if (shift == 0)
+    {
         return num;
     }
-    
+
     // Находим старшую и младшую часть числа
     int pow10_shift = pow(10, shift);
     int last_part = num % pow10_shift;
     int first_part = num / pow10_shift;
-    
+
     // Сдвигаем
     return last_part * (pow(10, digits - shift)) + first_part;
 }
 
-
-
 int Task()
 {
-    int a = 12;
-    int b = 1;
-    printf("Сдвиг вправо: %d", cyclic_right_shift(a, b));
+    int num = 0;
+    int shift = 0;
+
+    // Ввод числа и сдвига от пользователя
+    printf("Введите число (num): ");
+    scanf("%d", &num);
+
+    printf("Введите сдвиг (shift): ");
+    scanf("%d", &shift);
+
+    printf("Сдвиг вправо: %d", cyclic_right_shift(num, shift));
     return 0;
 }
